0614bd5ccc5f7624513da09d05881e9ff1051eb3
Fabio156 <fabio-vitoria1@hotmail.com>
2014-01-10 16:25:14 -0200

    Update editar.php

--- cadastro/editar.php
+++ cadastro/editar.php
@@ -10,10 +10,27 @@
      else{
         $id = $_REQUEST["id"];
         $nome = $_REQUEST["nome"];
+        $sexo = $_REQUEST["sexo"];
+        $estado = $_REQUEST["estado"];
+        $Observacoes = $_REQUEST["Observacoes"];
+        
+        $aceito = false;
+        if(isset($_REQUEST["aceito"])){
+        $aceito = true;
+        
+        }
+        
+        
+        $pessoa = array();
+        $pessoa["nome"] = $nome;
+        $pessoa["sexo"] = $sexo;
+        $pessoa["aceito"] = $aceito;
+        $pessoa["estado"] = $estado;
+        $pessoa["Observacoes"] = $Observacoes;
         
        
         $cadastros =& $_SESSION["cadastros"];
-        $cadastros[$id] = $nome;
+        $cadastros[$id] = $pessoa;
         echo "<br/>";
         echo "Edição Efetuada com Sucesso";
      }    
