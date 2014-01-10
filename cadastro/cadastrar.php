28c717fc331c54acd5f7811edd2786ba7cd68506
Fabio156 <fabio-vitoria1@hotmail.com>
2014-01-10 16:24:29 -0200

    Update cadastrar.php

--- cadastro/cadastrar.php
+++ cadastro/cadastrar.php
@@ -1,16 +1,28 @@
 <?php
     require_once("menu.php");
-
-
     session_start();
     
     if(!isset($_SESSION["cadastros"])){
         $_SESSION["cadastros"] = array();
-     }
-   
-   $nome = $_REQUEST["nome"];
-   
-   array_push($_SESSION["cadastros"], $nome);
-   echo "<br/>";
-   echo "Cadastro efetuado com Sucesso";
-?>
+    }
+    
+    $nome = $_REQUEST["nome"];
+    $sexo = $_REQUEST["sexo"];
+    $estado = $_REQUEST["Estado"];
+    $Observacoes = $_REQUEST["Observacoes"];
+    
+    $aceito = false;
+    if(isset($_REQUEST["aceito"])){
+        $aceito = true;
+    }    
+    
+    $pessoa = array();
+    $pessoa["nome"] = $nome;
+    $pessoa["sexo"] = $sexo;
+    $pessoa["aceito"] = $aceito;
+    $pessoa["estado"] = $estado;
+    $pessoa["Observacoes"] = $Observacoes;
+    
+    array_push($_SESSION["cadastros"], $pessoa);
+    echo "<br/>";
+    echo "Cadastro efetuado com sucesso!";
