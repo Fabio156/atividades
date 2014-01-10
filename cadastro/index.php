010b6e34580bfa58ae600922caa08d5e508739ad
Fabio156 <fabio-vitoria1@hotmail.com>
2014-01-10 16:27:30 -0200

    Update index.php

--- cadastro/index.php
+++ cadastro/index.php
@@ -1,3 +1,20 @@
+<style>
+    .masculino{
+        color: green;
+    }
+    .femenino{
+        color: yellow;
+    }
+    
+</style>
+
+<html>
+    <head>
+        <title>Cadastro de Pessoas</title>
+    </head>
+     <body background="londres_iStock_000005872948Medium.jpg">
+     </body>   
+
 <?php
     require_once("menu.php");
 
@@ -7,14 +24,39 @@
         
         $cadastros = $_SESSION["cadastros"];
         
+        echo "<dl>";
         foreach($cadastros as $pessoa){
+            $sexo = $pessoa["sexo"];
+            $aceito = $pessoa["aceito"];
+            $Observacoes = $pessoa["Observacoes"];
+            
+            
+            
             if($pessoa != null){
-                echo "$pessoa <br/> ";
+               echo "<dt class='$sexo'>" . $pessoa["nome"] . "</dt>";
+               echo "<dd>Sexo:" . $sexo . "</dd>";
+               echo "<dd>Estado: " . $pessoa["estado"] . "</dd>";
+               echo "<dd>Aceita o contrato?";
+               echo "<dd>Observacoes: " . $pessoa["observacoes"] . "</dd>";
+               
+               
+               
+               if($aceito){
+                    echo "</br>";
+                    echo "Usúario Aceita Contrato!";
+                }
+                else{
+                    echo "</br>";
+                    echo "O Usúario Não Aceita o Contrato!";
+                }
+                echo "</dd>";
             }    
         }
+        echo "</dl>";
     }
     else{
-        echo "Não existe pessoas cadastradas";
+        echo "<br/>";
+        echo "Não existe pessoas cadastradas!";
     }
 
 ?>
