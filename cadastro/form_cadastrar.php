e3da0f690dd19f49ea6979b1804e79f16d43c30e
Fabio156 <fabio-vitoria1@hotmail.com>
2014-01-10 16:25:41 -0200

    Update form_cadastrar.php

--- cadastro/form_cadastrar.php
+++ cadastro/form_cadastrar.php
@@ -2,12 +2,12 @@
     <head>
         <title>Cadastro de Pessoas</title>
     </head>
-     <body>
+     <body background="londres_iStock_000005872948Medium.jpg">
         <?php
             require_once("menu.php");
         ?>
         <form action="cadastrar.php" method="post">
-            <input type="text" name="nome" />
+            <?php require_once ("campos_pessoa.php"); ?>
             <input type="submit" value="Enviar" />
         </form>        
     </body>
