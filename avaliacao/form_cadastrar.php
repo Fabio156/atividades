566416aeefdc17133e850aab221cc1c2d8552cbb
Fabio156 <fabio-vitoria1@hotmail.com>
2014-01-13 17:48:59 -0200

    Create form_cadastrar.php

---
+++ avaliacao/form_cadastrar.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Cadastro de produtos </title>
+    </head>
+    <body>
+        <?php
+            require_once("menu.php");
+        ?>
+        <form action="cadastrar.php" method="post">
+            <label>Nome: </label>
+            <br/>
+            <?php require_once("campos_produto.php"); ?>
+            <input type="submit" value="Enviar" />
+        </form>
+    
+    </body>
+</html>
+
