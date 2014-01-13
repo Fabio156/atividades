70188a38bb134d7625544502923dc96e9e2d7965
Fabio156 <fabio-vitoria1@hotmail.com>
2014-01-13 17:48:24 -0200

    Create campus_produto.php

---
+++ avaliacao/campus_produto.php
@@ -0,0 +1,34 @@
+<style>
+    .Preto{
+        color: green;
+    }
+    .Amarelo{
+        color: yellow;
+    }
+    .Azul{
+        color: Blue;
+    }
+    
+</style>
+
+
+
+<input type="text" name="nome" />
+</br>
+<label>Preto </label>
+<input type="radio" name="produto" value="P" checked="checked" />
+<label>Azul</label>
+<input type="radio" name="cor" value="A" />
+<label>Amarelo</label>
+</br>
+<input type="checkbox" name="aceito" value="Sim" />
+<label>Produto</label>
+
+<br/>
+<label>Prazo de Entrega</label>
+<select name="Prazo de entrega">
+    <option>5 dias</option>
+    <option>10 dias</option>
+    <option>15 dias</option>
+</select>
+<br/>
